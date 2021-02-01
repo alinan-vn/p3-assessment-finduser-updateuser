a197715f64aaf46bdbc5fa13945ba712133d870d
Alejandro Linan <jandrolinan@gmail.com>
2021-02-01 15:46:47 -0600

    first commit

---
+++ src/main/java/org/hcl/SearchUserUpdateUser/SearchUserUpdateUserApplication.java
@@ -0,0 +1,13 @@
+package org.hcl.SearchUserUpdateUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchUserUpdateUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchUserUpdateUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hcl/SearchUserUpdateUser/SearchUserUpdateUserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hcl.SearchUserUpdateUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchUserUpdateUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
