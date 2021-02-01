4aa3b415e39eb6f03c587ce8b5c4b463e3d2067b
Alejandro Linan <jandrolinan@gmail.com>
2021-02-01 16:13:20 -0600

    updated controller, findUser.jsp

--- src/main/java/org/hcl/SearchUserUpdateUser/controller/AccountController.java
+++ src/main/java/org/hcl/SearchUserUpdateUser/controller/AccountController.java
@@ -1,5 +1,44 @@
 package org.hcl.SearchUserUpdateUser.controller;
 
+import org.hcl.SearchUserUpdateUser.crudRepository.UserEntityCrudRepository;
+import org.hcl.SearchUserUpdateUser.entity.UserEntity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class AccountController {
 
+	@Autowired
+	UserEntityCrudRepository userEntityCrudRepository;
+
+	@GetMapping(path = "/createAccount")
+	public String CreateAccountGet(Model model) {
+		
+		UserEntity newUser = new UserEntity();
+		model.addAttribute("newUser", newUser);
+		return "createAccount";
+	}
+	
+	@PostMapping(path = "/createAccount")
+	public void CreateAccountPost(@ModelAttribute("userFormData") UserEntity userEntity, BindingResult result) {
+
+		System.out.println("==== ACCOUNTCONTROLLER - CreateAccount");
+		System.out.println("==== Form Data: ");
+		System.out.println("==== Username: " + userEntity.getName());
+		System.out.println("==== Password: " + userEntity.getPassword());
+						
+		if (userEntity == null || userEntity.getName() == null) {
+			throw new RuntimeException("Name Required");
+		} 
+		if (userEntity.getPassword() == null) {
+			throw new RuntimeException("Password Required");
+		}
+		userEntityCrudRepository.save(userEntity);
+	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/findUser.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="ISO-8859-1">
+		<title>Find User</title>
+	</head>
+
+	<body>
+		<h1>Login!</h1>
+		<form action='' method='POST'>
+			<p>ID</p>
+			<input name='name' type='text' />
+			<br />
+			<input type='submit' />
+		</form>
+	</body>
+
+</html>
